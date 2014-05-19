e6b5d32818d2292e6570b917400d37fe18cbddbf
KimmoCommit <registerstuffhere@gmail.com>
2014-05-19 20:41:23 +0300

    siistitty sisluettelo.js

--- _site/assets/js/kansionavigointi.js
+++ _site/assets/js/kansionavigointi.js
@@ -1,3 +1,4 @@
+
 $(function(){
 	$(".ylikansio").click(function(e){
 		e.preventDefault();
--- _site/assets/js/sisluettelo.js
+++ _site/assets/js/sisluettelo.js
@@ -1,29 +1,38 @@
+function Sisluettelo(){
+	this.luettelo = LuoLuettelo();
+}
+
+function LuoLuettelo(){
+	$(".kansio-kontti").each(function(){
+		var alikansio_nimi = $(this).children(".alikansio-otsikko").text().trim();
+		var alikansio_kategoria = $(this).children(".alikansio-kategoria").text().trim();
+		var kansio_nimi = $(this).children(".kansio-otsikko").text().trim();
+		var luokka = "kansio-puu-linkki";
+		
+		if($(this).hasClass("alikansio")) {
+			var alikansio_nimi_replace = alikansio_nimi.replace(/ /g, '');
+			$(this).attr("id",alikansio_nimi_replace);
+			$(".kansio-puu-ul").append('<a href="#' + alikansio_nimi_replace + '" data-category="' + alikansio_kategoria + '" class="' + luokka + ' alikansio-puu-linkki ' + alikansio_kategoria + '"><li><div class="kansio-puu-alimerkki kansio-puu-merkki"><img style="float:left;" src="assets/img/little7.svg" />'+alikansio_nimi+'</div></li></a>'); 
+		}
+					//if-lause on tässä välttämätön!
+					else if($(this).children(".kansio-otsikko").text().length > 0){
+						var kansio_nimi_replace = kansio_nimi.replace(/ /g, '');
+						$(this).attr("id",kansio_nimi_replace);
+						$(".kansio-puu-ul").append('<a href="#' + kansio_nimi_replace + '" data-category="' + kansio_nimi_replace + '" class="' + luokka + ' ylikansio"><li><div class="kansio-puu-ylimerkki kansio-puu-merkki"><img style="float:left;" src="assets/img/little7.svg" />'+kansio_nimi+'</div></li></a>'); 
+					}
+					else {
+						return;	
+					}
+				});
+
+}
+
 
 $(function(){
-	$(".kansio-kontti").each(function(){
-		if($(this).hasClass("alikansio")) {
-								//var alikansionOtsikko = $(this).children(".alikansio-otsikko").text().trim();
-								var thisText = $(this).children(".alikansio-otsikko").text().trim();
-								var thisTrimmerText = $(this).children(".alikansio-otsikko").text().replace(/ /g, '').trim();
-								var Class="kansio-puu-linkki";
 
-								var kansioKategoria = $(this).children(".alikansio-kategoria").text().trim();
+	var UusiLuettelo = new Sisluettelo();
+	UusiLuettelo.luettelo;
 
-								$(this).attr("id",thisTrimmerText);
-								$(".kansio-puu-ul").append('<a href="#' + thisTrimmerText + '" data-category="' + kansioKategoria + '" class="' + Class + ' alikansio-puu-linkki ' + kansioKategoria + '"><li><div class="kansio-puu-alimerkki kansio-puu-merkki"><img style="float:left;" src="assets/img/little7.svg" />'+thisText+'</div></li></a>'); 
-							}
-							//if-lause on tässä välttämätön!
-							else if($(this).children(".kansio-otsikko").text().length > 0){
-								var thisText = $(this).children(".kansio-otsikko").text().trim();
-								var thisTrimmerText = $(this).children(".kansio-otsikko").text().replace(/ /g, '').trim();
-								var Class="kansio-puu-linkki";
-								$(this).attr("id",thisTrimmerText);
-								$(".kansio-puu-ul").append('<a href="#' + thisTrimmerText + '" data-category="' + thisTrimmerText + '" class="' + Class + ' ylikansio"><li><div class="kansio-puu-ylimerkki kansio-puu-merkki"><img style="float:left;" src="assets/img/little7.svg" />'+thisText+'</div></li></a>'); 
-							}
-							else {
-								return;	
-							}
-						});
 });
 
 
